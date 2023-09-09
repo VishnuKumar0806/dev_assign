c21ba37b4231b7c14e2f19399c635d966baf2474
VishnuKumar0806 <vishnukumarrajan0806@gmail.com>
2023-09-09 22:46:33 +0530

    changes in data retriving

--- src/Components/MenuPage.js
+++ src/Components/MenuPage.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import Profile from '../Images/food.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouseUser, faBorderAll, faHeart, faUserPlus, faBullhorn, faUser, faGraduationCap, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 export default function MenuPage() {
+
+    const listRef = useRef(null);
+    const spandetails = ['home', 'dashboard', 'donations', 'benificary profile', 'my campaingns', 'my profile','explore campaingns', 'apply for scholarship', 'log out'];
+    const icons = ['faHouseUser', 'faBorderAll', 'faHeart', 'faHeart', 'faUserPlus', 'faBullhorn', 'faUser', 'faGraduationCap', 'faRightFromBracket'];
+
+    const [clickedIcon, setClickedIcon] = useState('');
+
+    useEffect(()=>{
+        setClickedIcon(5)
+    },[])
+
     return (
         <div className="menu-bar">
             <div className="profile-details">
-                <img src={Profile} alt="user profile"/>
+                <img src={Profile} alt="user profile" />
                 <p>Vishnu kumar</p>
             </div>
-            <div className="menu-details">
-                <li><span><FontAwesomeIcon icon={faHouseUser} /></span>home</li>
-                <li><span><FontAwesomeIcon icon={faBorderAll} /></span>dashboard</li>
-                <li><span><FontAwesomeIcon icon={faHeart} /></span>donations</li>
-                <li><span><FontAwesomeIcon icon={faUserPlus} /></span>benificary profile</li>
-                <li><span><FontAwesomeIcon icon={faBullhorn} /></span>my campaingns</li>
-                <li><span><FontAwesomeIcon icon={faUser} /> </span>my profile</li>
-                <li><span><FontAwesomeIcon icon={faHeart} /></span>explore campaingns</li>
-                <li><span><FontAwesomeIcon icon={faGraduationCap} /></span>appy for scholarship</li>
-                <li><span><FontAwesomeIcon icon={faRightFromBracket} /></span>log out</li>
+            <div className="menu-details" ref={listRef}>
+                {
+                    spandetails.map((value, index) => {
+                        return (
+                            <li
+                                key={index}
+                                onClick={() => setClickedIcon(index)}
+                                style={{
+                                    color: clickedIcon === index ? 'gray' : 'white',
+                                    background: clickedIcon === index ? 'white' : 'gray'
+                                }}
+                            >
+                                <span>{value}</span>
+                            </li>
+                        )
+                    })
+                }
             </div>
         </div>
     )
--- src/Components/UserDetails.js
+++ src/Components/UserDetails.js
@@ -2,139 +2,75 @@ import React from "react";
 import Profile from '../Images/food.png';
 
 export default function UserDetails() {
+    const primary_deatils = {
+        'name': 'vishnu',
+        'age group': '20-30',
+        'contact': 9876543210,
+        'e mail': 'vishnu31@gmail.com',
+    }
+    const userdetails = {
+        'name': 'vishnu',
+        'city': 'madurai',
+        'zip code': 625012,
+        'state': 'tamil-nadu',
+        'country': 'india',
+        'religion': 'hindu',
+        'pan number': '0DFPS1402A',
+        'prefession': 'employee'
+    }
+    const other_details = {
+        'With To Be Anonymous': 'no',
+        'Facebook Profile Link': 'no',
+        'Linkedliin Profile Link': 'no',
+        'I Donate For Tax Befifits': 'no',
+        'I Wish To Connect With My Ward': 'no',
+        'I Wish To Get A Call . Notifications From Skopick': 'no',
+        'I Wish To Reccive 80G Certificate': 'no'
+    };
     return (
         <div className="user-details">
             <div className="user-details-head">
-                <img src={Profile} alt="user profile"/>
-                <span>
-                    <p>Name</p>
-                    <p><b>Vishnu kumar</b></p>
-                </span>
-                <span>
-                    <p>Age group</p>
-                    <p><b>20-30</b></p>
-                </span>
-                <span>
-                    <p>Contact</p>
-                    <p><b>+91 9658741231</b></p>
-                </span>
-                <span>
-                    <p>Email</p>
-                    <p><b>vishu0831</b></p>
-                </span>
+                <img src={Profile} alt="user profile" />
+                {
+                    Object.entries(primary_deatils).map(([value, key]) => {
+                        return (
+                            <span className="user-details-head-span">
+                                <p>{value}</p>
+                                <p>{key}</p>
+                            </span>
+                        )
+                    })
+                }
                 <span>
                     <button>Edit</button>
                 </span>
             </div>
             <div className="user-other-details">
                 <div className="sub-details">
-                    <span>
-                        <label>
-                            Address
-                        </label>
-                        <p>:</p>
-                        <p>19/11, nayakar street</p>
-                    </span>
-                    <span>
-                        <label>
-                            City
-                        </label>
-                        <p>:</p>
-                        <p>Turicorn</p>
-                    </span>
-                    <span>
-                        <label>
-                            Zip code / pin code
-                        </label>
-                        <p>:</p>
-                        <p>628092</p>
-                    </span>
-                    <span>
-                        <label>
-                            state
-                        </label>
-                        <p>:</p>
-                        <p>tamil nadu</p>
-                    </span>
-                    <span>
-                        <label>
-                            country
-                        </label>
-                        <p>:</p>
-                        <p>india</p>
-                    </span>
-                    <span>
-                        <label>
-                            religion
-                        </label>
-                        <p>:</p>
-                        <p>chirstianitiy</p>
-                    </span>
-                    <span>
-                        <label>
-                            PAN number
-                        </label>
-                        <p>:</p>
-                        <p>0DFPS9404A</p>
-                    </span>
-                    <span>
-                        <label>
-                            prefession
-                        </label>
-                        <p>:</p>
-                        <p>employee</p>
-                    </span>
+                    {
+                        Object.entries(userdetails).map(([value, key]) => {
+                            return (
+                                <span>
+                                    <label>{value}</label>
+                                    <p>:</p>
+                                    <p>{key}</p>
+                                </span>
+                            )
+                        })
+                    }
                 </div>
                 <div className="sub-details">
-                    <span>
-                        <label>
-                            with to be anonymous
-                        </label>
-                        <p>:</p>
-                        <p>no</p>
-                    </span>
-                    <span>
-                        <label>
-                            facebook profile link
-                        </label>
-                        <p>:</p>
-                        <p>-</p>
-                    </span>
-                    <span>
-                        <label>
-                            linkedliin profile link
-                        </label>
-                        <p>:</p>
-                        <p>-</p>
-                    </span>
-                    <span>
-                        <label>
-                            I donate for tax befifits
-                        </label>
-                        <p>:</p>
-                        <p>no</p>
-                    </span>
-                    <span>
-                        <label>
-                            I wish to connect with my ward
-                        </label>
-                        <p>:</p>
-                        <p>no</p>
-                    </span>
-                    <span>
-                        <label>
-                            I wish to get a call . notifications from skopick
-                        </label>
-                        <p>:</p>
-                        <p>no</p>
-                    </span>
-                    <span>
-                        <label>
-                            I wish to reccive 80G certificate
-                        </label>
-                        <p>:</p>
-                        <p>Yes</p>
-                    </span>
+                    {
+                        Object.entries(other_details).map(([value, key]) => {
+                            return (
+                                <span>
+                                    <label>{value}</label>
+                                    <p>:</p>
+                                    <p>{key}</p>
+                                </span>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
