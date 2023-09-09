751573068a487da435dfea7d2f5d372f4fe32e25
VishnuKumar0806 <vishnukumarrajan0806@gmail.com>
2023-09-09 20:17:05 +0530

    added comment lines

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+// importing react dependities
 import React from "react";
+
+// importing files
 import CompanyName from "./Components/CompanyName";
 import MenuPage from "./Components/MenuPage";
 import './Components/Style.css';
 import UserDetails from "./Components/UserDetails";
+
 function App() {
   return (
     <div className="main-page">
